6e764abba222fd3ae5ee1016b81142de2522ca06
Pradeepak5 <pradeepak571@gmail.com>
2022-04-01 19:27:29 +0530

    Scrolling used in flipcart

--- test_flipkart.py
+++ test_flipkart.py
@@ -11,13 +11,26 @@ def setUp():
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.maximize_window()
     yield
-    time.sleep(15)
+    time.sleep(5)
     driver.close()
     print("Tested Successfully")
 
 def test_search_product(setUp):
-    driver.get("https://www.flipkart.com/")
+    driver.get("https://www.google.com/")
+    time.sleep(1)
+    driver.find_element_by_name("q").send_keys("Flipkart")
+    time.sleep(1)
+    driver.find_element_by_name("btnK").click()
+    time.sleep(1)
+    driver.find_element_by_xpath("/html/body/div[7]/div/div[10]/div[1]/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/a/h3").click()
+    time.sleep(1)
     driver.find_element_by_xpath("/html/body/div[2]/div/div/button").click()
+    time.sleep(1)
     driver.find_element_by_name("q").send_keys(product)
+    time.sleep(1)
     driver.find_element_by_class_name("L0Z3Pu").click()
+    time.sleep(8)
+    driver.execute_script("window.scrollTo(0,window.scrollY+3500)")
+    time.sleep(8)
+    driver.close()
 
